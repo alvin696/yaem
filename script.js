eac2f6e9b486f87d10bc0cf4a9d4a0658dbdb248
Alvin kipkirui yaem <alvinyaem4@gmail.com>
2023-03-18 09:42:15 +0300

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+// Create a new h2 element
+const h2 = document.createElement("h2");
+
+// Add some text content to the h2 element
+h2.textContent = "This content added by JavaScript";
+
+// Append the h2 element to the body element in the DOM
+document.querySelector("body").appendChild(h2);
